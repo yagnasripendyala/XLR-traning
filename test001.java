ba48040052e1cb94bd6f7d3224184b19e40ee169
yagnasripendyala <yagnasripendyala@gmail.com>
2025-01-10 09:40:29 +0530

    Add files via upload

---
+++ test001.java
@@ -0,0 +1,11 @@
+class test001{
+    Run Debug
+    public static void main(string[] args) {
+        int n = 10;
+        for(i == 5 || i == 7) {
+            continue;
+        }
+        System.out.println(i);
+    }
+}
+}
